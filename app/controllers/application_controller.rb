2bfb17dd27c5e17a1a8b742d0cf9dd92b72b8c08
Vijay Ramamurthy <vrama628@gmail.com>
2024-03-04 13:38:33 -0600

    fix bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   def current_user
-    User.new(id: 1, name: 'Alice')
+    User.new(id: 0, name: 'Alice')
   end
   helper_method :current_user
 end
--- config/initializers/oso.rb
+++ config/initializers/oso.rb
@@ -5,12 +5,12 @@ $oso = OsoCloud::Oso.new(
 )
 $oso.policy(IO.read(Rails.root.join('config', 'oso.polar')))
 $oso.tell('has_role',
-  OsoCloud::Value.new(type: 'User', id: 'Alice'),
+  OsoCloud::Value.new(type: 'User', id: '0'),
   'owner',
   OsoCloud::Value.new(type: 'Project', id: '0')
 )
 $oso.tell('has_role',
-  OsoCloud::Value.new(type: 'User', id: 'Bob'),
+  OsoCloud::Value.new(type: 'User', id: '1'),
   'owner',
   OsoCloud::Value.new(type: 'Project', id: '1')
 )
